292be7076b6e28ea70354b822546c1d2a3f7935a
fred <fredlacs@gmail.com>
2021-09-17 12:04:53 +0100

    Fix subgraph

--- packages/layer2-token-gateway/src/mapping.ts
+++ packages/layer2-token-gateway/src/mapping.ts
@@ -1,9 +1,4 @@
-import {
-  DefaultGatewayUpdated as DefaultGatewayUpdatedEvent,
-  GatewaySet as GatewaySetEvent,
-  TransferRouted as TransferRoutedEvent,
-  TxToL1 as TxToL1Event,
-} from "../generated/L2GatewayRouter/L2GatewayRouter";
+import { GatewaySet as GatewaySetEvent } from "../generated/L2GatewayRouter/L2GatewayRouter";
 import { Gateway, Token, TokenGatewayJoinTable } from "../generated/schema";
 import { Address } from "@graphprotocol/graph-ts";
 
@@ -12,19 +7,26 @@ const addressToId = (input: Address): string =>
 
 export function handleGatewaySet(event: GatewaySetEvent): void {
   const gatewayId = addressToId(event.params.gateway);
+  const tokenId = addressToId(event.params.l1Token);
+  const joinId = gatewayId.concat(tokenId);
+
+  if (event.params.gateway == Address.zero()) {
+    // TODO: handle gateways being deleted
+    return;
+  }
+
   // TODO: right now we create new gateway since its faster than loading
   // if we do stateful stuff here in the future, this needs to instead load
   let gatewayEntity = new Gateway(gatewayId);
   gatewayEntity.save();
 
-  const tokenId = addressToId(event.params.gateway);
   // TODO: same issue as gateway above. should load instead of create new
   let tokenEntity = new Token(tokenId);
   // TODO: query gateway for L2 address
   tokenEntity.l2Address = null;
   tokenEntity.save();
 
-  let joinEntity = new TokenGatewayJoinTable(gatewayId.concat(tokenId));
+  let joinEntity = new TokenGatewayJoinTable(joinId);
   joinEntity.gateway = gatewayId;
   joinEntity.token = tokenId;
   joinEntity.save();
