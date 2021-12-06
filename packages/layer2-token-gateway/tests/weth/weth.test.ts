f4dba78198b4412fa5b91b39fe64b34ec73c4a3c
fred <fredlacs@gmail.com>
2021-12-06 10:40:26 +0000

    Add unit test disabling then enabling the token in the router

--- packages/layer2-token-gateway/tests/weth/weth.test.ts
+++ packages/layer2-token-gateway/tests/weth/weth.test.ts
@@ -6,6 +6,8 @@ import { GatewaySet as GatewaySetEvent } from "../../generated/L2GatewayRouter/L
 import { DepositFinalized as DepositFinalizedEvent } from "../../generated/templates/L2ArbitrumGateway/L2ArbitrumGateway";
 import { Gateway, Token, TokenGatewayJoinTable } from "../../generated/schema";
 
+const DISABLED_GATEWAY_ADDR = Address.fromString("0x0000000000000000000000000000000000000001");
+
 const createGatewaySet = (token: Address, gateway: Address): GatewaySetEvent => {
     let mockEvent = newMockEvent();
   
@@ -15,15 +17,17 @@ const createGatewaySet = (token: Address, gateway: Address): GatewaySetEvent => 
     
     parameters.push(tokenParam);
     parameters.push(gatewayParam);
-
-    let tx = mockEvent.transaction
-    tx.to = gateway
   
     let newGatewayEvent = new GatewaySetEvent(mockEvent.address, mockEvent.logIndex, mockEvent.transactionLogIndex,
-        mockEvent.logType, mockEvent.block, tx, parameters)
+        mockEvent.logType, mockEvent.block, mockEvent.transaction, parameters)
   
     return newGatewayEvent
-  }
+}
+
+const createDisabledGatewayEvent = (token: Address): GatewaySetEvent => {
+    return createGatewaySet(token, DISABLED_GATEWAY_ADDR)
+}
+
 const createDepositFinalized = (token: Address, gateway: Address): DepositFinalizedEvent => {
     let mockEvent = newMockEvent();
   
@@ -59,6 +63,64 @@ test("Can process gateway set event followed by deposit", () => {
         addressToId(gatewayAddr),
         addressToId(tokenAddr)
     )
+
+    let joinTableRes1 = TokenGatewayJoinTable.load(joinTableId)
+    if(!joinTableRes1) throw new Error("No join table found")
+
+    assert.stringEquals(
+        joinTableRes1.gateway.toString(),
+        gatewayAddr.toHexString()
+    )
+    assert.stringEquals(
+        joinTableRes1.token.toString(),
+        tokenAddr.toHexString()
+    )
+    
+    let newDepositFinalizedEvent1 = createDepositFinalized(tokenAddr, gatewayAddr);
+    handleDeposit(newDepositFinalizedEvent1);
+    
+    let joinTableRes2 = TokenGatewayJoinTable.load(joinTableId)
+    if(!joinTableRes2) throw new Error("No join table found")
+
+    assert.stringEquals(
+        joinTableRes2.gateway.toString(),
+        gatewayAddr.toHexString()
+    )
+    assert.stringEquals(
+        joinTableRes2.token.toString(),
+        tokenAddr.toHexString()
+    )
+})
+
+test("Can disable then enable gateway", () => {
+    const tokenAddr = Address.fromString("0xc778417e063141139fce010982780140aa065431")
+    const gatewayAddr = Address.fromString("0xf94bc045c4e926cc0b34e8d1c41cd7a0145156a5")
+    const joinTableId = getJoinId(
+        addressToId(gatewayAddr),
+        addressToId(tokenAddr)
+        )
+    const disabledJoinTableId = getJoinId(
+        addressToId(DISABLED_GATEWAY_ADDR),
+        addressToId(tokenAddr)
+    )
+    
+    let newGatewaySetEvent0 = createDisabledGatewayEvent(tokenAddr)
+    handleGatewaySet(newGatewaySetEvent0)
+
+    let joinTableRes0 = TokenGatewayJoinTable.load(disabledJoinTableId)
+    if(!joinTableRes0) throw new Error("No join table found")
+
+    assert.stringEquals(
+        joinTableRes0.gateway.toString(),
+        DISABLED_GATEWAY_ADDR.toHexString()
+    )
+
+    assert.stringEquals(
+        joinTableRes0.token.toString(),
+        tokenAddr.toHexString()
+    )
+    let newGatewaySetEvent1 = createGatewaySet(tokenAddr, gatewayAddr)
+    handleGatewaySet(newGatewaySetEvent1)
 
     let joinTableRes1 = TokenGatewayJoinTable.load(joinTableId)
     if(!joinTableRes1) throw new Error("No join table found")
