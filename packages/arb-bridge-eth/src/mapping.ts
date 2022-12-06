3c0e7216f5ddd7252de4b54c63ab187e4e100410
Goran Vladika <goran.vladika@gmail.com>
2022-12-06 20:34:14 +0100

    Track all the gateway router events

--- packages/arb-bridge-eth/src/mapping.ts
+++ packages/arb-bridge-eth/src/mapping.ts
@@ -9,13 +9,18 @@ import {
   IRollupCoreNodeConfirmed as NodeConfirmedEvent,
   IRollupCoreNodeRejected as NodeRejectedEvent,
 } from "./interface/IRollupCore";
-import {
+import {  
+  DefaultGatewayUpdated,
   OutboxEntry,
   OutboxOutput,
   Retryable,
   RawMessage,
   Node as NodeEntity,
   EthDeposit,
+  GatewaySet,
+  TransferRouted,
+  TxToL2,
+  WhitelistSourceUpdated,
 } from "../generated/schema";
 import {
   Bytes,
@@ -29,6 +34,13 @@ import {
   ByteArray
 } from "@graphprotocol/graph-ts";
 import { encodePadded, padBytes } from "@arbitrum/subgraph-common";
+import {
+  DefaultGatewayUpdated as DefaultGatewayUpdatedEvent,
+  GatewaySet as GatewaySetEvent,
+  TransferRouted as TransferRoutedEvent,
+  TxToL2 as TxToL2Event,
+  WhitelistSourceUpdated as WhitelistSourceUpdatedEvent
+} from "../generated/L1GatewayRouter/L1GatewayRouter"
 
 const getL2ChainId = (): Bytes => {
   const network = dataSource.network();
@@ -312,5 +324,56 @@ export function handleNodeRejected(event: NodeRejectedEvent): void {
   }
   entity.timestampStatusUpdate = event.block.timestamp
   entity.status = "Rejected"
+  entity.save()
+}
+
+export function handleDefaultGatewayUpdated(
+  event: DefaultGatewayUpdatedEvent
+): void {
+  let entity = new DefaultGatewayUpdated(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  )
+  entity.newDefaultGateway = event.params.newDefaultGateway
+  entity.save()
+}
+
+export function handleGatewaySet(event: GatewaySetEvent): void {
+  let entity = new GatewaySet(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  )
+  entity.l1Token = event.params.l1Token
+  entity.gateway = event.params.gateway
+  entity.save()
+}
+
+export function handleTransferRouted(event: TransferRoutedEvent): void {
+  let entity = new TransferRouted(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  )
+  entity.token = event.params.token
+  entity._userFrom = event.params._userFrom
+  entity._userTo = event.params._userTo
+  entity.gateway = event.params.gateway
+  entity.save()
+}
+
+export function handleTxToL2(event: TxToL2Event): void {
+  let entity = new TxToL2(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  )
+  entity._from = event.params._from
+  entity._to = event.params._to
+  entity._seqNum = event.params._seqNum
+  entity._data = event.params._data
+  entity.save()
+}
+
+export function handleWhitelistSourceUpdated(
+  event: WhitelistSourceUpdatedEvent
+): void {
+  let entity = new WhitelistSourceUpdated(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  )
+  entity.newSource = event.params.newSource
   entity.save()
 }
