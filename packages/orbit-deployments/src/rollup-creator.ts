4e3ff914ef9c36510de09fd69cb3139beb2557a9
Goran Vladika <goran.vladika@gmail.com>
2023-06-23 12:11:51 +0200

    Fetch more rollup info

--- packages/orbit-deployments/src/rollup-creator.ts
+++ packages/orbit-deployments/src/rollup-creator.ts
@@ -1,8 +1,10 @@
 import { Address, BigInt, log } from "@graphprotocol/graph-ts";
-import { RollupCreated } from "../generated/RollupCreator/RollupCreator";
+import { RollupCreated as RollupCreatedEvent } from "../generated/RollupCreator/RollupCreator";
+import { Rollup as RollupContract } from "../generated/RollupCreator/Rollup";
+import { ProxyAdmin as ProxyAdminContract } from "../generated/RollupCreator/ProxyAdmin";
 import { Rollup, RollupCreator } from "../generated/schema";
 
-export function handleRollupCreated(event: RollupCreated): void {
+export function handleRollupCreated(event: RollupCreatedEvent): void {
   const rollupCreator = getOrCreateRollupCreator(event.address);
   rollupCreator.totalRollupsCreated = rollupCreator.totalRollupsCreated.plus(BigInt.fromI32(1));
   rollupCreator.save();
@@ -12,6 +14,17 @@ export function handleRollupCreated(event: RollupCreated): void {
   rollup.adminProxy = event.params.adminProxy;
   rollup.bridge = event.params.bridge;
   rollup.sequencerInbox = event.params.sequencerInbox;
+
+  // fetch remaining info from contract
+  const rollupContract = RollupContract.bind(Address.fromBytes(rollup.id));
+  rollup.outbox = rollupContract.outbox();
+  rollup.chainId = rollupContract.chainId();
+  rollup.baseStake = rollupContract.baseStake();
+  rollup.latestConfirmed = rollupContract.latestConfirmed();
+  rollup.rollupDeploymentBlock = rollupContract.rollupDeploymentBlock();
+  rollup.stakeToken = rollupContract.stakeToken();
+  rollup.owner = rollupContract.owner();
+
   rollup.save();
 }
 
