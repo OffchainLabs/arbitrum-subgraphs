14fbab17f42b29530e42ae571723421b90e08f3e
Fionna Chan <13184582+fionnachan@users.noreply.github.com>
2024-01-03 13:42:29 +0000

    feat: add sepolia subgraphs (#30)
    * feat: add sepolia subgraphs (WIP)
    
    * add sepolia
    
    * remove graph-cli package and update packages
    
    * update package versions
    
    * update hex string
    
    * update readme

--- packages/arb-bridge-eth/src/utils.ts
+++ packages/arb-bridge-eth/src/utils.ts
@@ -34,8 +34,8 @@ export const getL2ChainId = (): Bytes => {
     }
   }
 
-  if (network == "rinkeby") return Bytes.fromByteArray(Bytes.fromHexString("0x066EEB"));
   if (network == "goerli") return Bytes.fromByteArray(Bytes.fromHexString("0x066eed"));
+  if (network == "sepolia") return Bytes.fromByteArray(Bytes.fromHexString("0x066eee"));
 
   log.critical("No chain id recognised", []);
   throw new Error("No chain id found");
