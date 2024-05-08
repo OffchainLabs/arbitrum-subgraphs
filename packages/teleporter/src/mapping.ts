345e4e0ed45bfc91242a5afdc7ce5bc4a2076916
Henry <11198460+godzillaba@users.noreply.github.com>
2024-05-08 15:02:52 -0400

    start

---
+++ packages/teleporter/src/mapping.ts
@@ -0,0 +1,14 @@
+import { Teleported as TeleportedEvent } from "../generated/L1Teleporter/L1Teleporter";
+
+import { Teleported } from "../generated/schema";
+
+export function handleTeleported(event: TeleportedEvent): void {
+  let entity = new Teleported(event.transaction.hash.toHex() + "-" + event.logIndex.toString())
+  entity.sender = event.params.sender
+  entity.l1Token = event.params.l1Token
+  entity.l3FeeTokenL1Addr = event.params.l3FeeTokenL1Addr
+  entity.l1l2Router = event.params.l1l2Router
+  entity.l2l3RouterOrInbox = event.params.l2l3RouterOrInbox
+  entity.to = event.params.to
+  entity.amount = event.params.amount
+}
